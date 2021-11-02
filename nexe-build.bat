02d1cfa5e8d3c58d231c20d63d69a85eeb807874
RDPStudio <67402215+rdp-studio@users.noreply.github.com>
2021-11-02 21:16:31 +0800

    Update nexe-build.bat

--- nexe-build.bat
+++ nexe-build.bat
@@ -1,6 +1,5 @@
 rem nexe hexa-node.js --target win32-x86-9.8.0 --ico icon.ico --name hexa -o hexa --build
 nexe hexa-node.js --target windows-x64-14.15.3 --name hexa -o hexa
 rem set ResourceHacker="ResourceHacker.exe"
-@echo off
 echo Replacing icon...
 ResourceHacker.exe -addoverwrite "hexa.exe", "hexa.exe", "icon.ico", ICONGROUP, MAINICON, 0
